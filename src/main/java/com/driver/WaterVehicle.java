b4c3e54187f642a93af58cf238f0ed9f5748d91f
Sanket Ghorpade <69021392+SankyGhorpade@users.noreply.github.com>
2023-06-10 13:57:11 +0530

    Update WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,23 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+// Boat class implementing WaterVehicle interface
+class Boat implements WaterVehicle {
+    String name;
+    int capacity;
+    
+    // Constructor
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    
+    // Implementing the methods of WaterVehicle interface
+    public String getVehicleName() {
+        return this.name;
+    }
+    
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
