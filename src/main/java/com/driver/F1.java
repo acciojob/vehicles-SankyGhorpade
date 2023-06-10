f3140c18ba8b571c47f4d585663c13add0f1848f
Sanket Ghorpade <69021392+SankyGhorpade@users.noreply.github.com>
2023-06-10 14:11:54 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,44 +2,30 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual, int currentGear) {
-         super(name, currentGear);
-        this.isManual = isManual;
+    public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,2,6,isManual,"type1",2);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-           int newSpeed = super.currentSpeed + rate;
-        if (newSpeed <= 250) {
-            super.currentSpeed = newSpeed;
-             if (newSpeed <= 50)
-                super.currentGear = 1;
-            else if (newSpeed <= 100)
-                super.currentGear = 2;
-            else if (newSpeed <= 150)
-                super.currentGear = 3;
-            else if (newSpeed <= 200)
-                super.currentGear = 4;
-            else if (newSpeed <= 250)
-                super.currentGear = 5;
-        }
-        else {
-            super.currentGear = 6;
-        }
-        /**
-         * speed 0: gear 1
-         * speed 1-50: gear 1
-         * speed 51-100: gear 2
-         * speed 101-150: gear 3
-         * speed 151-200: gear 4
-         * speed 201-250: gear 5
-         * speed more than 250: gear 6
-         */
+        int newSpeed = super.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
 
         if(newSpeed == 0) {
-            super.currentGear = 1;
             //Stop the car, set gear as 1
+            super.stop();
+            super.changeGear(1);
+        }else if(newSpeed >=1 && newSpeed <=50){
+            super.changeGear(1); //gear as 1
+        }else if(newSpeed >=51 && newSpeed <=100){
+            super.changeGear(2); //gear as 2
+        }else if(newSpeed >=101 && newSpeed <=150){
+            super.changeGear(3); //gear as 3
+        }else if(newSpeed >=151 && newSpeed <=200){
+            super.changeGear(4); //gear as 4
+        }else if(newSpeed >=201 && newSpeed <=250){
+            super.changeGear(5); //gear as 5
+        }else{
+            super.changeGear(6); //gear as 6
         }
         //for all other cases, change the gear accordingly
 
